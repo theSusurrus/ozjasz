12e22bf0e6bdeeef7d56258b0d3f03db3f76a5f3
Jakub Dudarewicz <jakub.dudarewicz@harman.com>
2020-01-08 12:17:09 +0100

    Refactor bigotry engine

--- generate.py
+++ generate.py
@@ -7,27 +7,27 @@ def get_phrase(phrase_list):
         num_phrases = len(phrase_list)
         return phrase_list[random.randint(0,num_phrases-1)]
 
-def protocol_1_percent(phrase, speed):
-    print("")
+def construct_sentence():
+    sentence = get_phrase(list1) + " " \
+             + get_phrase(list2) + " " \
+             + get_phrase(list3) + " " \
+             + get_phrase(list4) + ", "\
+             + get_phrase(list5) + " " \
+             + get_phrase(list6) + "."
+    return sentence
+
+def protocol_1_percent(phrase, speed, speak=True):
     print(phrase)
 
-    engine = pyttsx3.init('espeak')
+    if(speak):
+        engine = pyttsx3.init('espeak')
+        engine.setProperty('voice', 'polish')
+        engine.setProperty('rate', speed)
+        engine.say(phrase)
+        engine.runAndWait()
+        engine.stop()
 
-    engine.setProperty('voice', 'polish')
-    engine.setProperty('rate', speed)
-    engine.say(phrase)
-    engine.runAndWait()
-    engine.stop()
-
-phrase = ""
-
-phrase = phrase + get_phrase(list1) + " "
-phrase = phrase + get_phrase(list2) + " "
-phrase = phrase + get_phrase(list3) + " "
-phrase = phrase + get_phrase(list4) + ", "
-phrase = phrase + get_phrase(list5) + " "
-phrase = phrase + get_phrase(list6) + "."
-
-#protocol_1_percent("Aktywowano protokół jeden procent...", 150)
-protocol_1_percent(phrase, 150)
+protocol_1_percent(phrase="Aktywowano protokół jeden procent...", speed=150, speak=False)
+for i in range(10):
+    protocol_1_percent(phrase=construct_sentence(), speed=150, speak=False)
     
